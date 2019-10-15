e7114328828e03dc446302f92fab24d6b561e88e
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-10-15 21:09:20 +0900

    fix : express-graphql -> graphql-yoga 로 변경
    좀 더 쉬운 방식을 찾다가 라이브러리 변경

--- server/app.js
+++ server/app.js
@@ -4,15 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const controller = require('./controller/controller');
 const models = require('./models/index');
+// graphql
+const {GraphQLServer} = require('graphql-yoga');
+const resolvers = require('./resolvers/user-resolvers');
+
 models.sequelize.sync().then(()=>{
   console.log("DB Start");
 }).catch(err=>{
   console.error(err);
 });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -24,25 +27,49 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/public')));
+// app.use(express.static(path.join(__dirname, '../client/public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const context = req =>{
+  req : req.request
+}
+const server = new GraphQLServer({
+  typeDefs : 'graphql/schema.graphql',
+  resolvers,
+  context
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const options = {
+  port : 3333,
+  endpoint : '/api',
+  playground : '/play',
+  cors:{
+    credentials : true,
+    origin : ["http://localhost:3000"]
+  }
+}
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
+
+
+server.start(options,()=>console.log("Graphql Server start"));
+app.use('/', controller);
+
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
