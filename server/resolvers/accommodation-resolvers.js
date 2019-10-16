12814f52a366b29fbfd0785b97ddfaa8b7ecb740
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-10-16 18:17:07 +0900

    feat : 인원수 설정에 따라 원하는 방이 나오게 API 구현

--- server/resolvers/accommodation-resolvers.js
+++ server/resolvers/accommodation-resolvers.js
@@ -1,20 +1,34 @@
-require('dotenv').config();
+require("dotenv").config();
 const models = require("../models/index");
+const {Op} = require('sequelize');
 // const passport = require("passport");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const resolvers = {
-    Query:{
-        accommodations : async (root,value,context,info)=>{
-            const accs = await models.accommodation.findAll();
-            return accs;
-        },
-        accommodation : async (root,{id},context,info)=>{
-            const acc = await models.accommodation.findOne({
-                where : {id}
-            });
-            return acc;
+  Query: {
+    accommodations: async (root, value, context, info) => {
+      const accs = await models.accommodation.findAll();
+      return accs;
+    },
+    accommodation: async (root, { id }, context, info) => {
+      const acc = await models.accommodation.findOne({
+        where: { id }
+      });
+      return acc;
+    },
+    findAccForNumberOfPerson: async (root, { count }, context, info) => {
+        const accs = await models.accommodation.findAll({
+        where: {
+          min_person: {
+            [Op.lte]: count
+          },
+          max_person: {
+            [Op.gte]: count
+          }
         }
+      });
+      return accs;
     }
+  }
 };
 
 module.exports = resolvers;
