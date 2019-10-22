549b9727e2f9c25f8a552c4318f7728b12a2a501
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-10-22 21:44:12 +0900

    feat : Route를 이용한 Route 만들기
    여러 페이지를 Route 하기 위해서 사용

--- client/airbnb/src/App.js
+++ client/airbnb/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Login from './views/login/index';
-import Index from './views/index/index'
+import Index from './views/index/index';
+import Homes from './views/homes/index';
 import {Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Index}/>
       <Route path="/signin" component={Login} />
-      <Route path="/homes"/>
+      <Route path="/homes" component={Homes}/>
     </div>
   );
 }
